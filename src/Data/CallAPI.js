f1eb38051347f77a6617d3cd4d4b213a524169da
askari-mahsa <mahsa.ba.askari@gmail.com>
2022-09-10 16:54:22 +0430

    doing Edit this part

--- src/Data/CallAPI.js
+++ src/Data/CallAPI.js
@@ -3,3 +3,7 @@ export const callAPI = () => {
 	let baseURL = "https://jsonplaceholder.typicode.com/users";
 	return axios.get(baseURL);
 };
+export const callAPIEdit = (userid) => {
+	let baseURL = "https://jsonplaceholder.typicode.com/posts/" + userid;
+	return axios.get(baseURL);
+};
